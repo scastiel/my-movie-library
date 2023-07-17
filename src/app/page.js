e0222f12cf13d13df66591b92aff7dd115d0ddb7
Sebastien Castiel <sebastien@castiel.me>
2023-07-16 20:27:57 -0400

    Lesson: jsx

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,18 @@
 export default function HomePage() {
-  return <p>Coming soon: a movie library app!</p>
+  return (
+    <main>
+      <h1>My Movie Library</h1>
+      <p>Here is a list of popular movies:</p>
+      <ul>
+        <li>Indiana Jones and the Dial of Destiny</li>
+        <li>Guardians of the Galaxy Vol. 3</li>
+        <li>Mission: Impossible - Dead Reckoning Part One</li>
+        <li>Spider-Man: Across the Spider-Verse</li>
+        <li>The Super Mario Bros. Movie</li>
+      </ul>
+      <p>
+        <a href="https://www.themoviedb.org/">See more</a>
+      </p>
+    </main>
+  )
 }
