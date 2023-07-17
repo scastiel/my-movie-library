e9352bc44ab369074343e63fb6c06a623eb73d56
Sebastien Castiel <sebastien@castiel.me>
2023-07-16 20:34:21 -0400

    Lesson: arrays

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,22 @@ export default function HomePage() {
   const appTitle = 'My Movie Library'
   const hasMoreMovies = true
 
+  const movies = [
+    { id: 0, title: 'Indiana Jones and the Dial of Destiny' },
+    { id: 1, title: 'Guardians of the Galaxy Vol. 3' },
+    { id: 2, title: 'Mission: Impossible - Dead Reckoning Part One' },
+    { id: 3, title: 'Spider-Man: Across the Spider-Verse' },
+    { id: 4, title: 'The Super Mario Bros. Movie' },
+  ]
+
   return (
     <main>
       <h1>{appTitle}</h1>
       <p>Here is a list of popular movies:</p>
       <ul className="movie-list">
-        <li className="featured">Indiana Jones and the Dial of Destiny</li>
-        <li>Guardians of the Galaxy Vol. 3</li>
-        <li>Mission: Impossible - Dead Reckoning Part One</li>
-        <li>Spider-Man: Across the Spider-Verse</li>
-        <li>The Super Mario Bros. Movie</li>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
       </ul>
       <p>
         {hasMoreMovies ? (
