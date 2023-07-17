c94d8b373de1275b49efafc3a46b8d15b9be74c2
Sebastien Castiel <sebastien@castiel.me>
2023-07-16 20:59:44 -0400

    Lesson: fetching-api

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,23 +1,21 @@
 import { MovieList } from '../../components/movie-list'
+import { fetchPopularMovies } from '../../lib/tmdb'
 
 export const metadata = {
   title: 'Movies',
 }
 
-export default function MoviesPage() {
-  const movies = [
-    { id: 0, title: 'Indiana Jones and the Dial of Destiny' },
-    { id: 1, title: 'Guardians of the Galaxy Vol. 3' },
-    { id: 2, title: 'Mission: Impossible - Dead Reckoning Part One' },
-    { id: 3, title: 'Spider-Man: Across the Spider-Verse' },
-    { id: 4, title: 'The Super Mario Bros. Movie' },
-  ]
+export default async function MoviesPage() {
+  const movies = await fetchPopularMovies()
 
   return (
     <>
       <h1>Popular Movies</h1>
-
-      <MovieList movies={movies} />
+      {movies !== null ? (
+        <MovieList movies={movies} />
+      ) : (
+        <p>An error occurred 🙁</p>
+      )}
 
       <p>
         <a href="https://www.themoviedb.org/">See more</a>
