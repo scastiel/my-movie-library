41cd8646ebbc697f4a010c09a279fde344107f89
Sebastien Castiel <sebastien@castiel.me>
2023-07-16 20:42:22 -0400

    Lesson: css-modules

--- src/components/movie-list-item.js
+++ src/components/movie-list-item.js
@@ -1,3 +1,5 @@
+import styles from './movie-list-item.module.css'
+
 export function MovieListItem({ movie }) {
-  return <li>{movie.title}</li>
+  return <li className={styles.movieListItem}>{movie.title}</li>
 }
