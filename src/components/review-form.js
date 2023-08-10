7f16cbc9eb1d0ff5319287d64aede9316b0f8bc0
Sebastien Castiel <sebastien@castiel.me>
2023-08-10 09:08:21 -0400

    Lesson: validation

--- src/components/review-form.js
+++ src/components/review-form.js
@@ -3,6 +3,38 @@ import { useState } from 'react'
 import styles from './review-form.module.css'
 import { StarRating } from './star-rating'
 
+function validateReviewFormValues(formValues) {
+  const errors = {}
+
+  if (!formValues.name) {
+    errors.name = 'Please enter a name.'
+  } else if (formValues.name.length < 3) {
+    errors.name = 'Please enter at least 3 characters.'
+  } else if (formValues.name.length > 20) {
+    errors.name = 'Please enter at most 20 characters.'
+  }
+
+  if (!formValues.email) {
+    errors.email = 'Please enter an email address.'
+  } else if (!formValues.email.match(/^.+@.+$/)) {
+    errors.email = 'Please enter a valid email address.'
+  }
+
+  if (!formValues.rating || formValues.rating <= 0 || formValues.rating > 5) {
+    errors.rating = 'Please select a rating.'
+  }
+
+  if (!formValues.review) {
+    errors.review = 'Please enter a review.'
+  } else if (formValues.review.length < 10) {
+    errors.review = 'Please enter at least 10 characters.'
+  } else if (formValues.review.length > 500) {
+    errors.review = 'Please enter at most 500 characters.'
+  }
+
+  return errors
+}
+
 export function ReviewForm({ movie }) {
   const [formValues, setFormValues] = useState({
     name: '',
@@ -10,10 +42,17 @@ export function ReviewForm({ movie }) {
     rating: null,
     review: '',
   })
+  const [errors, setErrors] = useState({})
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log('Submit form:', formValues)
+
+    const errors = validateReviewFormValues(formValues)
+    setErrors(errors)
+    const hasErrors = Object.keys(errors).length > 0
+    if (!hasErrors) {
+      console.log('Submit form:', formValues)
+    }
   }
 
   return (
@@ -32,6 +71,7 @@ export function ReviewForm({ movie }) {
             })
           }
         />
+        {errors.name && <div className={styles.error}>{errors.name}</div>}
       </section>
       <section>
         <label htmlFor="email">Your friend’s email address</label>
@@ -46,6 +86,7 @@ export function ReviewForm({ movie }) {
             })
           }
         />
+        {errors.email && <div className={styles.error}>{errors.email}</div>}
       </section>
       <section>
         <label>Your rating</label>
@@ -55,6 +96,7 @@ export function ReviewForm({ movie }) {
             setFormValues({ ...formValues, rating: Number(event.target.value) })
           }
         />
+        {errors.rating && <div className={styles.error}>{errors.rating}</div>}
       </section>
       <section>
         <label htmlFor="review">Your review</label>
@@ -68,6 +110,7 @@ export function ReviewForm({ movie }) {
             })
           }
         />
+        {errors.review && <div className={styles.error}>{errors.review}</div>}
       </section>
       <div>
         <button type="submit">Send</button>
