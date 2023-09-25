6401a0cc060c2b5ca98fc1f01fb5596d18d5dedb
Sebastien Castiel <sebastien@castiel.me>
2023-09-25 11:39:13 -0400

    Lesson: processing

--- src/app/movies/[movieId]/submit-review/route.js
+++ src/app/movies/[movieId]/submit-review/route.js
@@ -1,6 +1,40 @@
 import { NextResponse } from 'next/server'
+import { validateReviewFormValues } from '../../../../lib/validation'
+import { Resend } from 'resend'
+import { fetchMovie } from '../../../../lib/tmdb'
 
-export async function POST(req) {
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function POST(req, { params }) {
+  const movie = await fetchMovie(params.movieId)
+  if (!movie) {
+    return NextResponse.json({ success: false }, { status: 404 })
+  }
+
   const formData = await req.json()
+  const errors = validateReviewFormValues(formData)
+  const hasErrors = Object.keys(errors).length > 0
+  if (hasErrors) {
+    return NextResponse.json({ success: false, errors }, { status: 422 })
+  }
+
+  try {
+    const stars = '⭐️'.repeat(formData.rating)
+    const payload = {
+      from: `My Movie Library <onboarding@resend.dev>`,
+      to: [formData.email],
+      subject: `${formData.name}’s review of ${movie.title}: ${stars}`,
+      html: `
+        <p>${formData.name} just sent you a review of <strong>${movie.title}</strong>:</p>
+        <p>Rating: ${stars}</p>
+        <blockquote>${formData.review}</blockquote>
+      `,
+    }
+    await resend.emails.send(payload)
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ success: false }, { status: 500 })
+  }
+
   return NextResponse.json({ success: true, formData })
 }
--- src/components/review-form.js
+++ src/components/review-form.js
@@ -2,38 +2,7 @@
 import { useState } from 'react'
 import styles from './review-form.module.css'
 import { StarRating } from './star-rating'
-
-function validateReviewFormValues(formValues) {
-  const errors = {}
-
-  if (!formValues.name) {
-    errors.name = 'Please enter a name.'
-  } else if (formValues.name.length < 3) {
-    errors.name = 'Please enter at least 3 characters.'
-  } else if (formValues.name.length > 20) {
-    errors.name = 'Please enter at most 20 characters.'
-  }
-
-  if (!formValues.email) {
-    errors.email = 'Please enter an email address.'
-  } else if (!formValues.email.match(/^.+@.+$/)) {
-    errors.email = 'Please enter a valid email address.'
-  }
-
-  if (!formValues.rating || formValues.rating <= 0 || formValues.rating > 5) {
-    errors.rating = 'Please select a rating.'
-  }
-
-  if (!formValues.review) {
-    errors.review = 'Please enter a review.'
-  } else if (formValues.review.length < 10) {
-    errors.review = 'Please enter at least 10 characters.'
-  } else if (formValues.review.length > 500) {
-    errors.review = 'Please enter at most 500 characters.'
-  }
-
-  return errors
-}
+import { validateReviewFormValues } from '../lib/validation'
 
 export function ReviewForm({ movieId }) {
   const [formValues, setFormValues] = useState({
